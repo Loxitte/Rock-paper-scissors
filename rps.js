2b96a178283f48b68f028bb77c39ecd6bc1e18c4
Lottie <242483042+Loxitte@users.noreply.github.com>
2026-02-10 13:39:08 -0500

    Add code to generate and classify random number (unfinished).

--- rps.js
+++ rps.js
@@ -1,1 +1,27 @@
 console.log("Hello World")
+
+function getComputerChoice() {
+    let randomNumber = Math.random;
+
+    let result;
+
+    if(randomNumber <= 0.333) {
+        result = "Rock";
+    } else if(randomNumber >= 0.667) {
+        result = "Scissors";
+    } else {
+        result = "Paper";
+    }
+
+    return result;
+    }
+
+    console.log(getComputerChoice);
+//Generate random number from 0 to 1//
+
+//IF RN is less than or equal to 0.333, assign value "rock"//
+
+//IF RN greater than or equal to 0.667, assign value "scissors"//
+
+//ELSE, assign value "Paper"//
+
