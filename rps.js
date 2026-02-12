9fe2a81c4b0fe63f0298e33864c471ac8b7d357d
Lottie <242483042+Loxitte@users.noreply.github.com>
2026-02-12 14:57:53 -0500

    Add random number generator and assign values for rock paper and scissors.

--- rps.js
+++ rps.js
@@ -1,16 +1,22 @@
 console.log("Hello World")
 
-function randomInt() {
-    return Math.floor(Math.random() * 3);
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3)
+    switch (randomNumber) {
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "paper";
+            break;
+        case 2:
+            return "scissors";
+            break;
+    }
 }
-
-console.log(randomInt())
-//Generate random integer from 0 to 2//
-
-
-//IF RI is equal to 0, assign value "rock"//
-
-//IF RI equal to 1 assign value "scissors"//
-
-//ELSE, assign value "Paper"//
+console.log (getComputerChoice())
+//Generate a random integer from 0 to 2
+//IF RI is equal to 0, assign value "rock"
+//IF RI equal to 1 assign value "scissors"
+//ELSE, assign value "Paper"
 
