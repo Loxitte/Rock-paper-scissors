8b224962899dc32496d373cf8563d350b8e07348
Lottie <242483042+Loxitte@users.noreply.github.com>
2026-02-17 10:27:30 -0500

    Add variables and function playRound. Function playRound does not yet work as intended. Variables added are humanScore and computerScore.

--- rps.js
+++ rps.js
@@ -21,7 +21,7 @@ console.log (getComputerChoice())
 //ELSE, assign value "Paper"
 
 function getHumanChoice() {
-    prompt("Rock, paper, or scissors?")
+    return(prompt("Rock, paper, or scissors?").toLowerCase());
 }
 console.log(getHumanChoice())
 //Prompt user to enter value, rock paper or scissors
@@ -33,3 +33,37 @@ let computerScore = 0
 console.log (humanScore)
 console.log (computerScore)
 
+let humanchoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+
+function playRound() {
+if (humanchoice == computerChoice) {
+    return ("Tie!");
+}
+else if ((humanchoice == "rock") && (computerChoice == "scissors")) {
+    return ("Hooman wins!");
+    humanScore++;
+}
+else if ((humanchoice== "paper") && (computerChoice == "rock")) {
+    return ("Hooman wins!");
+    humanScore++;
+}
+else if ((humanchoice == "scissors") && (computerChoice == "paper")) {
+    return ("Hooman wins!");
+    humanScore++;
+}
+else if ((computerChoice == "rock") && (humanchoice == "scissors")) {
+    return ("Compooter wins!");
+    computerScore++;
+}
+else if ((computerChoice == "paper") && (humanchoice == "rock")) {
+    return ("Compooter wins!");
+    computerScore++;
+}
+else if ((computerChoice == "scissors") && (humanchoice == "paper")) {
+    return ("Compooter wins!");
+    computerScore++;
+}}
+console.log (playRound());
+console.log ("Human Score: " + humanScore);
+console.log ("Computer Score: " + computerScore);
